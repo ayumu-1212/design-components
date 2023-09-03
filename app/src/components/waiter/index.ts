f970cde75fde17dbcd1a78120d943c63e006f0da
ayumu-1212 <ayumuabe1434@gmail.com>
2023-09-03 22:27:31 +0900

    ✍️ useTransitionのためのwaiterを作成

---
+++ app/src/components/waiter/index.ts
@@ -0,0 +1,1 @@
+export { waiter } from './waiter'
---
+++ app/src/components/waiter/waiter.ts
@@ -0,0 +1,5 @@
+export const waiter = async (s: number) => {
+  const sleep = (s: number) => new Promise((resolve) => setTimeout(resolve, s))
+  await sleep(s)
+  return
+}
